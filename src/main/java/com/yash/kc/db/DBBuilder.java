f8f263083a06c1515f31857b607cede40462f4cb
Keshav C Chaure <keshav.chaure@Yash.com>
2018-05-14 18:46:01 +0530

    some formating

--- src/main/java/com/yash/kc/db/DBBuilder.java
+++ src/main/java/com/yash/kc/db/DBBuilder.java
@@ -10,19 +10,19 @@ import com.yash.kc.domain.Address;
 import com.yash.kc.resource.DerbyConnection;
 
 public class DBBuilder {
- 
+
 	private static String createADDRESSBOOKTbl = "CREATE TABLE ADDRESSBOOKTbl (NAME VARCHAR(32) NOT NULL, ADDRESS VARCHAR(50) NOT NULL)";
-	private static String inserSQL="insert into ADDRESSBOOKTbl values (?,?)";
-	
-	public static void main(String[] args) throws ClassNotFoundException, SQLException{
-		
-	//	DBScript.createTable(createADDRESSBOOKTbl);	   
-	    HashMap<String, String> keyValue =new HashMap<String, String>();
-	    keyValue.put("ravi", "p");	     
-	    keyValue.put("raju", "u");	
-	    keyValue.put("rana", "s"); 
-	    DBScript.addData(inserSQL, keyValue);
-		
+	private static String inserSQL = "insert into ADDRESSBOOKTbl values (?,?)";
+
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		// DBScript.createTable(createADDRESSBOOKTbl);
+		HashMap<String, String> keyValue = new HashMap<String, String>();
+		keyValue.put("ravi", "p");
+		keyValue.put("raju", "u");
+		keyValue.put("rana", "s");
+		DBScript.addData(inserSQL, keyValue);
+
 	}
 
 }
--- src/main/java/com/yash/kc/jdbc_demo/App.java
+++ src/main/java/com/yash/kc/jdbc_demo/App.java
@@ -10,62 +10,54 @@ import com.yash.kc.db.DBScript;
 import com.yash.kc.domain.Address;
 import com.yash.kc.resource.DerbyConnection;
 
-public class App  {
+public class App {
 
- 
 	private static Connection conn = null;
 
-	public static void main(String[] args) throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-		
+	public static void main(String[] args)
+			throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
 
-		 
-		    if (args.length != 2) {
-		      System.out.println("Usage: java JavaDBDemo <Name> <Address>");
-		     // System.exit(1);
-		    }
-		//    String driver = "org.apache.derby.jdbc.EmbeddedDriver";
-	//	    String driver = "org.apache.derby.jdbc.ClientDriver";
-	//	    String dbName = "AddressBookDB";
-	//	    String connectionURL = "jdbc:derby:testdb1;create=true";
-	//	    String createString = "CREATE TABLE ADDRESSBOOKTbl (NAME VARCHAR(32) NOT NULL, ADDRESS VARCHAR(50) NOT NULL)";
-	//	    Class.forName(driver);
+		// String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+		// String driver = "org.apache.derby.jdbc.ClientDriver";
+		// String dbName = "AddressBookDB";
+		// String connectionURL = "jdbc:derby:testdb1;create=true";
+		// String createString = "CREATE TABLE ADDRESSBOOKTbl (NAME VARCHAR(32)
+		// NOT NULL, ADDRESS VARCHAR(50) NOT NULL)";
+		// Class.forName(driver);
 
-	//	    conn = DriverManager.getConnection(connectionURL);
-		    
-	/*	    Statement stmt = conn.createStatement();
-		    stmt.executeUpdate(createString);
-*/
-	  /*
-		    PreparedStatement psInsert = conn
-		        .prepareStatement("insert into ADDRESSBOOKTbl values (?,?)");
+		// conn = DriverManager.getConnection(connectionURL);
 
-		    psInsert.setString(1, "some");
-		    psInsert.setString(2, "Pune");
+		/*
+		 * Statement stmt = conn.createStatement();
+		 * stmt.executeUpdate(createString);
+		 */
+		/*
+		 * PreparedStatement psInsert = conn
+		 * .prepareStatement("insert into ADDRESSBOOKTbl values (?,?)");
+		 * 
+		 * psInsert.setString(1, "some"); psInsert.setString(2, "Pune");
+		 * 
+		 * psInsert.executeUpdate();
+		 */
 
-		    psInsert.executeUpdate();
-   */
-		    
-		    /*
-		    conn=DerbyConnection.getDerbyConnection();
-		    Statement stmt2 = conn.createStatement();
-		    ResultSet rs = stmt2.executeQuery("select * from ADDRESSBOOKTbl");
-		    System.out.println("Addressed present in your Address Book\n\n");
-		    int num = 0;
+		/*
+		 * conn=DerbyConnection.getDerbyConnection(); Statement stmt2 =
+		 * conn.createStatement(); ResultSet rs =
+		 * stmt2.executeQuery("select * from ADDRESSBOOKTbl");
+		 * System.out.println("Addressed present in your Address Book\n\n"); int
+		 * num = 0;
+		 * 
+		 * while (rs.next()) { System.out.println(++num + ": Name: " +
+		 * rs.getString(1) + "\n Address" + rs.getString(2)); }
+		 * 
+		 * rs.close();
+		 */
 
-		    while (rs.next()) {
-		      System.out.println(++num + ": Name: " + rs.getString(1) + "\n Address"
-		          + rs.getString(2));
-		    }
-		   
-		    rs.close();
-		      */
-		    
-		    
-		    conn=DerbyConnection.getDerbyConnection();
-		    AddressDaoImpl address=new AddressDaoImpl();
-		    List<Address> listAddresses=address.getAllAddresses();
-		    listAddresses.stream().forEach(s->System.out.println(s.getCity() +"  "+s.getName()));
-		    System.out.print("list of address "+ listAddresses);
-		    
-		  }
+		conn = DerbyConnection.getDerbyConnection();
+		AddressDaoImpl address = new AddressDaoImpl();
+		List<Address> listAddresses = address.getAllAddresses();
+		listAddresses.stream().forEach(s -> System.out.println(s.getCity() + "  " + s.getName()));
+		System.out.print("list of address " + listAddresses);
+
+	}
 }
